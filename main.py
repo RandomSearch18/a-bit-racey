c956c195081c992668a8975e008b90439246c644
RandomSearch18 <101704343+RandomSearch18@users.noreply.github.com>
2023-11-14 13:04:57 +0000

    Add a car with a cool texture

--- main.py
+++ main.py
@@ -1,18 +1,42 @@
 import pygame
+from pathlib import Path
 
 pygame.init()
-game_display = pygame.display.set_mode((800, 400))
+
+DISPLAY_WIDTH = 800
+DISPLAY_HEIGHT = 400
+
+# Colours
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+
+# Images
+CAR_IMAGE = pygame.image.load(Path("assets", "racecar.png"))
+
+# Game parts
+game_display = pygame.display.set_mode((DISPLAY_WIDTH, DISPLAY_HEIGHT))
 pygame.display.set_caption('A bit Racey')
-
 clock = pygame.time.Clock()
-
 crashed = False
+
+
+# Game objects
+def draw_car(x: float, y: float):
+    game_display.blit(CAR_IMAGE, (x, y))
+
+
+car_x = DISPLAY_WIDTH * 0.45
+car_y = DISPLAY_HEIGHT * 0.8
+
 while not crashed:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             crashed = True
 
         print(event)
+
+    game_display.fill(WHITE)
+    draw_car(car_x, car_y)
 
     pygame.display.update()
     clock.tick(60)
