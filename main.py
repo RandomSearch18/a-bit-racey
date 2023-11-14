2523aa89af1e0109b81b4b2a36210c2bab0b0636
RandomSearch18 <101704343+RandomSearch18@users.noreply.github.com>
2023-11-14 14:28:33 +0000

    Start making it use classses

--- main.py
+++ main.py
@@ -1,45 +1,73 @@
 import pygame
+import math
 from pathlib import Path
 
-pygame.init()
 
-DISPLAY_WIDTH = 800
-DISPLAY_HEIGHT = 400
-
-# Colours
-BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
-
-# Images
-CAR_IMAGE = pygame.image.load(Path("assets", "racecar.png"))
-
-# Game parts
-game_display = pygame.display.set_mode((DISPLAY_WIDTH, DISPLAY_HEIGHT))
-pygame.display.set_caption('A bit Racey')
-clock = pygame.time.Clock()
-crashed = False
+class Asset:
+    CAR_IMAGE = pygame.image.load(Path("assets", "racecar.png"))
 
 
-# Game objects
-def draw_car(x: float, y: float):
-    game_display.blit(CAR_IMAGE, (x, y))
+class Color:
+    BLACK = (0, 0, 0)
+    WHITE = (255, 255, 255)
 
 
-car_x = DISPLAY_WIDTH * 0.45
-car_y = DISPLAY_HEIGHT * 0.8
+class Game:
 
-while not crashed:
+    def __init__(self):
+        # Window display dimensions
+        self.WIDTH = 800
+        self.HEIGHT = 400
+
+        # Initilise the display surface
+        self.surface = pygame.display.set_mode((self.WIDTH, self.HEIGHT))
+        pygame.display.set_caption('A bit Racey')
+
+        # Initialise other game components
+        self.clock = pygame.time.Clock()
+        self.has_crashed = False
+
+        pygame.init()
+
+
+class Car:
+
+    def calculate_starting_x(self):
+        car_height = self.texture.get_height()
+        screen_height = self.game.HEIGHT
+
+        return screen_height - car_height
+
+    def calculate_starting_y(self):
+        screen_height = self.game.WIDTH
+        return math.floor(screen_height / 2)
+
+    def __init__(self, game: Game):
+        self.game = game
+        self.texture = Asset.CAR_IMAGE
+
+        self.x = self.calculate_starting_x()
+        self.y = self.calculate_starting_y()
+
+    def draw(self):
+        self.game.surface.blit(self.texture, (self.x, self.y))
+
+
+game = Game()
+car = Car(game)
+
+while not game.has_crashed:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             crashed = True
 
         print(event)
 
-    game_display.fill(WHITE)
-    draw_car(car_x, car_y)
+    game.surface.fill(Color.WHITE)
+    car.draw()
 
     pygame.display.update()
-    clock.tick(60)
+    game.clock.tick(60)
 
 pygame.quit()
 quit()
